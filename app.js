bbb1ebce31c269343be0960a4534c00862ade413
Sahabiar <sahabiar43@gmail.com>
2025-02-26 00:46:51 +0600

    changes added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // Code for see more button
 const seeMore = document.getElementById('btn');
-const hidden = document.getElementById('hidden');
+document.getElementById('hidden').style.display = 'none';
 
-seeMore.addEventListener('click', () => {
-  hidden.classList.toggle('hidden');
+
+seeMore.addEventListener('click', function () {
+  const hidden = document.getElementById('hidden');
+
+  if (hidden.style.display === 'none') {
+    hidden.style.display = 'block';
+    seeMore.innerHTML = 'See Less';
+  }
+  else {
+    hidden.style.display = 'none';
+    seeMore.innerHTML = 'See More';
+  }
 });
