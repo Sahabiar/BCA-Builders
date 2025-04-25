e1796d6c54881305a29a9ddf44b11f06085b2088
Sahabiar <sahabiar43@gmail.com>
2025-04-25 18:10:39 +0600

    changes added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('hidden').style.display = 'none';
 
 seeMore.addEventListener('click', function () {
   const hidden = document.getElementById('hidden');
-
+  
   if (hidden.style.display === 'none') {
     hidden.style.display = 'block';
     seeMore.innerHTML = 'See Less';
